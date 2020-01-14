05caa544ddc1965830c37d7b485609194b37fe5f
barbaraZakrz <34478263+barbaraZakrz@users.noreply.github.com>
2020-01-14 23:44:25 +0100

    com3

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,14 +1,37 @@
-plik = open("grid.txt", "r")
+from math import *
 listaD = []
-for i in range(0, 20, 1):
-    wiersz = []
-    wiersz = plik.readline()
-    lista = []
-    for j in range(0, 39, 2):
-        lista.append(int(wiersz[j]))
-    #print(lista)
-    #print("----------------------------------------")
-    listaD.append(lista)
+
+class PierwszaKlasa:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.h = sqrt((x-20)**2 + (y-20)**2)
+
+#wczytanie mapy
+def wczytanie():
+    plik = open("grid.txt", "r")
+    for i in range(0, 20, 1):
+        wiersz = []
+        wiersz = plik.readline()
+        lista = []
+        for j in range(0, 39, 2):
+            lista.append(int(wiersz[j]))
+        #print(lista)
+        #print("----------------------------------------")
+        listaD.append(lista)
+    #print(listaD)
+    listaD.reverse()
+    print(listaD)
+
+def wyswietl():
+    for i in range(0, 20, 1):
+        wiersz = ""
+        for j in range(0,20,1):
+            wiersz+=str(listaD[i][j])
+            wiersz+=" "
+        print(wiersz)
+
+wczytanie()
+wyswietl()
 
 
-print(listaD)
