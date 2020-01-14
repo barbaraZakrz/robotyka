4071537d572f29c29826b0f9ca6d3e00bdae7ace
barbaraZakrz <34478263+barbaraZakrz@users.noreply.github.com>
2020-01-15 00:22:56 +0100

    com4

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,13 +1,18 @@
 from math import *
+
 listaD = []
 
 class PierwszaKlasa:
-    def __init__(self, x, y):
+    def __init__(self, x, y, g, rodzic):
         self.x = x
         self.y = y
-        self.h = sqrt((x-20)**2 + (y-20)**2)
+        self.h = sqrt((x - 19) ** 2 + (y - 19) ** 2)
+        self.g = g
+        self.f = g + self.h
+        self.rodzic = rodzic
 
-#wczytanie mapy
+
+# wczytanie mapy
 def wczytanie():
     plik = open("grid.txt", "r")
     for i in range(0, 20, 1):
@@ -16,22 +21,30 @@ def wczytanie():
         lista = []
         for j in range(0, 39, 2):
             lista.append(int(wiersz[j]))
-        #print(lista)
-        #print("----------------------------------------")
+        # print(lista)
+        # print("----------------------------------------")
         listaD.append(lista)
-    #print(listaD)
+    # print(listaD)
     listaD.reverse()
     print(listaD)
 
+# wyswietlenie mapy jako macierz
 def wyswietl():
     for i in range(0, 20, 1):
         wiersz = ""
-        for j in range(0,20,1):
-            wiersz+=str(listaD[i][j])
-            wiersz+=" "
+        for j in range(0, 20, 1):
+            wiersz += str(listaD[i][j])
+            wiersz += " "
         print(wiersz)
+
+def TworzyObiekty(rodzic):
+    if rodzic.x +1 >= 0 and rodzic.x +1 < 20:
+        if rodzic.y >= 0 and rodzic.y < 20:
+            if listaD[rodzic.x+1][rodzic.y] == 0:
+                PierwszaKlasa(rodzic.x+1, rodzic.y, rodzic.g+1, rodzic)
 
 wczytanie()
 wyswietl()
-
-
+otwarta = []
+zamknieta = []
+zamknieta.append(PierwszaKlasa(0,0,0,None))
